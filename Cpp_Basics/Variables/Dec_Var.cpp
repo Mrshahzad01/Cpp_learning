e29715044b02465cc7b8466ca297f5c1616ace88
Muhammad Shahzad <mrshahzad7602@gmail.com>
2025-09-06 22:15:09 +0500

    Add new topic Variables

---
+++ Cpp_Basics/Variables/Dec_Var.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+//       Syntax 
+//   type variableName = value;
+
+//  Create a variable called myNum of type int and assign it the value 15:
+
+    int myNum = 15;
+    cout<<myNum<<endl;
+
+//   Now declare a variable without assigning the value, and assign the value later:
+    int my_num;
+    my_num = 30;
+    cout<<my_num<<endl;
+
+
+// Changing Variable Value
+    float MyNum = 23.0;
+    MyNum = 40.0;
+    cout<<"Previouse value is 23.0 and Now the new value is 40.0"<<MyNum;
+
+    return 0;
+}
+
