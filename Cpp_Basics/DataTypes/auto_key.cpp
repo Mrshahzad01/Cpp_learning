731b5cf12bc9f64ece1e4c710d6cdf333ee2be33
Muhammad Shahzad <mrshahzad7602@gmail.com>
2025-09-23 23:31:15 +0500

    Add New topic(DataTypes) with 1 Example

---
+++ Cpp_Basics/DataTypes/auto_key.cpp
@@ -0,0 +1,14 @@
+/*
+The auto keyword automatically detects the type of a variable based on the value you assign to it.
+It helps you write cleaner code and avoid repeating types, especially for long or complex types.
+For example: Instead of writing int x = 5;, you can write:
+*/
+
+#include <iostream>
+using namespace std;
+ 
+int main () {
+  auto x = 5 ,y = 12;   // x is automatically treated as int   
+  cout << x + y;
+  return 0;
+}
---
+++ Cpp_Basics/DataTypes/data_type.cpp
@@ -0,0 +1,29 @@
+//   DataTypes
+
+// 1. As explained in the Variables chapter, a variable in C++ must be a specified data type.
+// 2. The data type specifies the size and type of information the variable will store.
+
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+int main () {
+  // Creating variables
+  int myNum = 5;               // Integer (whole number)
+  float myFloatNum = 5.99;     // Floating point number
+  double myDoubleNum = 9.98;   // Floating point number
+  char myLetter = 'D';         // Character
+  bool myBoolean = true;       // Boolean
+  string myString = "Hello Ali";   // String
+   
+  // Print variable values
+  cout << "int: " << myNum << "\n";
+  cout << "float: " << myFloatNum << "\n";
+  cout << "double: " << myDoubleNum << "\n";
+  cout << "char: " << myLetter << "\n";
+  cout << "bool: " << myBoolean << "\n";
+  cout << "string: " << myString << "\n";
+ 
+  return 0;
+}
+
